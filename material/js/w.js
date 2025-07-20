1bcab26973d856c0200f67921657215a4a28eee6
MovemDimon <movem9013@gmail.com>
2025-07-20 13:44:34 +0330

    Update w.js

--- material/js/w.js
+++ material/js/w.js
@@ -1,7 +1,10 @@
-window.triggerWalletWarning = function () {
-  if (typeof showNotification === 'function') {
-    showNotification("❌ امکان اتصال ولت شما تا تاریخ 2025/09/09 مقدور نیست!", "error");
-  } else {
-    console.warn("showNotification is not ready yet!");
+document.addEventListener('DOMContentLoaded', function () {
+  const btn = document.querySelector('.task-button');
+
+  if (btn) {
+    btn.addEventListener('click', function (e) {
+      e.preventDefault();
+      showNotification("❌ امکان اتصال ولت شما تا تاریخ 2025/09/09 مقدور نیست!", "error");
+    });
   }
-};
+});
