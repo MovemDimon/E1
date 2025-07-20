b96e66293abbdd94053b3cec5bfd03770ec521e2
MovemDimon <movem9013@gmail.com>
2025-07-20 12:42:50 +0330

    Update w.js

--- material/js/w.js
+++ material/js/w.js
@@ -1,17 +1,9 @@
-function getQueryParam(param) {
-  const urlParams = new URLSearchParams(window.location.search);
-  return urlParams.get(param);
-}
-
-// تابع نمایش نوتیفیکیشن
 function showNotification(message) {
-  // اگر مرورگر از Notification پشتیبانی نمی‌کند
   if (!("Notification" in window)) {
     alert(message);
     return;
   }
 
-  // اگر اجازه داده نشده
   if (Notification.permission !== "granted") {
     Notification.requestPermission().then(permission => {
       if (permission === "granted") {
@@ -25,8 +17,7 @@ function showNotification(message) {
   }
 }
 
-// بررسی پارامتر و نمایش پیام مناسب
-const reason = getQueryParam("w");
-if (reason === "wallet_blocked") {
+// تابعی که به دکمه وصل میشه
+function triggerWalletBlocked() {
   showNotification("امکان اتصال ولت شما تا تاریخ 2025/09/09 مقدور نیست!");
 }
